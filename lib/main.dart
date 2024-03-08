089ee341e53f052d20d19255965a14e3cfff4dbd
shubhangrao10 <shubhangrao10@gmail.com>
2024-03-08 12:42:46 +0530

    adding files of the project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_healthcare_app/src/config/route.dart';
+import 'package:flutter_healthcare_app/src/theme/theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Health Care',
+      theme: AppTheme.lightTheme,
+      routes: Routes.getRoute(),
+      onGenerateRoute: (settings) =>(child: Routes.onGenerateRoute(settings)),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "SplashPage",
+    );
+  }
+}
